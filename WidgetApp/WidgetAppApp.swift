cd7747b66d8d90c4fbb99a13f26bc5b9717aa71c
Tanya Simmer <101064028+tatkagore@users.noreply.github.com>
2023-11-23 10:40:11 +0100

    Initial Commit

---
+++ WidgetApp/WidgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetAppApp.swift
+//  WidgetApp
+//
+//  Created by Tatiana Simmer on 23/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct WidgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
